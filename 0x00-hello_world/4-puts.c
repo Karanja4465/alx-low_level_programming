56ca29fe6acabb8c5c5bda47828ec1073142dd83
Karanja4465 <mcliljohnz@gmail.com>
2022-06-30 14:52:31 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * Return: 0 after printing the funtion
  */
 int main(void)
-{\*body of main function*/
-	puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
